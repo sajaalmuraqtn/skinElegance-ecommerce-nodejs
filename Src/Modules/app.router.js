fedce9602ab716c9f435c2700074d93126f3b472
sajaalmuraqtn <sajaalmuraqtn@gmail.com>
2024-04-12 18:09:03 +0300

    deploy project

--- Src/Modules/app.router.js
+++ Src/Modules/app.router.js
@@ -10,12 +10,12 @@ import CartRouter from './Cart/cart.router.js'
 import OrderRouter from './Order/order.router.js'
 import FavoriteRouter from './Favorite/favorite.router.js'
 import AdvertisementRouter from './Advertisement/advertisement.router.js'
-
+import cors from 'cors'
 import ConnectDB from '../../DB/connection.js';
 
 const initApp=(app,express)=>{
-
-app.use(express.json());
+    app.use(express.json());
+    app.use(cors())
 
 app.get('/',(req,res)=>{
     return res.json('welcome...')
