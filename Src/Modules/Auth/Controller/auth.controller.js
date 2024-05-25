e73c38c65ff6c52cdc83426c65d481b00b12a287
sajaalmuraqtn <sajaalmuraqtn@gmail.com>
2024-05-25 16:06:11 +0300

    edit the logo in send email

--- Src/Modules/Auth/Controller/auth.controller.js
+++ Src/Modules/Auth/Controller/auth.controller.js
@@ -27,7 +27,9 @@ export const signUp = async (req, res,next) => {
       
     const slug = slugify(userName);
     const token = jwt.sign({ email }, process.env.CONFIRMEMAILSECRET);
-    await sendEmail(email, "Confirm Email", `
+    await sendEmail(email, "Confirm Email",` <!DOCTYPE html>
+    <html>
+    
     <head>
         <title></title>
         <!--[if !mso]><!-- -->
@@ -35,20 +37,61 @@ export const signUp = async (req, res,next) => {
         <!--<![endif]-->
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <style type="text/css">
-            #outlook a { padding: 0; }
-            .ReadMsgBody { width: 100%; }
-            .ExternalClass { width: 100%; }
-            .ExternalClass * { line-height: 100%; }
-            body { margin: 0; padding: 0; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; background-color: #fafafa; }
-            table, td { border-collapse: collapse; mso-table-lspace: 0pt; mso-table-rspace: 0pt; }
-            img { border: 0; height: auto; line-height: 100%; outline: none; text-decoration: none; -ms-interpolation-mode: bicubic; }
-            p { display: block; margin: 13px 0; }
+            #outlook a {
+                padding: 0;
+            }
+    
+            .ReadMsgBody {
+                width: 100%;
+            }
+    
+            .ExternalClass {
+                width: 100%;
+            }
+    
+            .ExternalClass * {
+                line-height: 100%;
+            }
+    
+            body {
+                margin: 0;
+                padding: 0;
+                -webkit-text-size-adjust: 100%;
+                -ms-text-size-adjust: 100%;
+                background-color: #fafafa;
+            }
+    
+            table,
+            td {
+                border-collapse: collapse;
+                mso-table-lspace: 0pt;
+                mso-table-rspace: 0pt;
+            }
+    
+            img {
+                border: 0;
+                height: auto;
+                line-height: 100%;
+                outline: none;
+                text-decoration: none;
+                -ms-interpolation-mode: bicubic;
+            }
+    
+            p {
+                display: block;
+                margin: 13px 0;
+            }
         </style>
         <!--[if !mso]><!-->
         <style type="text/css">
             @media only screen and (max-width:480px) {
-                @-ms-viewport { width: 320px; }
-                @viewport { width: 320px; }
+                @-ms-viewport {
+                    width: 320px;
+                }
+    
+                @viewport {
+                    width: 320px;
+                }
             }
         </style>
         <!--<![endif]-->
@@ -62,9 +105,12 @@ export const signUp = async (req, res,next) => {
         <![endif]-->
         <!--[if lte mso 11]>
         <style type="text/css">
-            .outlook-group-fix { width: 100% !important; }
+            .outlook-group-fix {
+                width:100% !important;
+            }
         </style>
         <![endif]-->
+    
         <!--[if !mso]><!-->
         <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700" rel="stylesheet" type="text/css">
         <style type="text/css">
@@ -73,98 +119,81 @@ export const signUp = async (req, res,next) => {
         <!--<![endif]-->
         <style type="text/css">
             @media only screen and (min-width:480px) {
-                .mj-column-per-100, *[aria-labelledby="mj-column-per-100"] { width: 100% !important; }
+                .mj-column-per-100,
+                * [aria-labelledby="mj-column-per-100"] {
+                    width: 100% !important;
+                }
             }
         </style>
     </head>
-    <body style="background: #fafafa;">
-        <div style="background-color:#fafafa;">
-            <!--[if mso | IE]>
-            <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="640" align="center" style="width:640px;">
-                <tr>
-                    <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
-            <![endif]-->
-            <style type="text/css">
-                html, body, * { -webkit-text-size-adjust: none; text-size-adjust: none; }
-                a { color: #FFC587; text-decoration: none; }
-                a:hover { text-decoration: underline; }
-            </style>
-            <div style="margin:0px auto;max-width:640px;background:transparent;">
-                <table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:transparent;" align="center" border="0">
-                    <tbody>
-                        <tr>
-                            <td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:40px 0px;">
-                                <!--[if mso | IE]>
-                                <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="640" align="center" style="width:640px;">
-                                    <tr>
-                                        <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
-                                <![endif]-->
-                                <div style="margin:0px auto;max-width:640px;background:#fafafa;">
-                                    <table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:#fafafa;" align="center" border="0">
-                                        <tbody>
-                                            <tr>
-                                                <td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:40px 70px;">
-                                                    <!--[if mso | IE]>
-                                                    <table role="presentation" border="0" cellpadding="0" cellspacing="0">
-                                                        <tr>
-                                                            <td style="vertical-align:top;width:640px;">
-                                                    <![endif]-->
-                                                    <div aria-labelledby="mj-column-per-100" class="mj-column-per-100 outlook-group-fix" style="vertical-align:top;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%;">
-                                                        <table role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
-                                                            <tbody>
-                                                                <tr>
-                                                                    <td style="word-break:break-word;font-size:0px;padding:0px 0px 20px;" align="left">
-                                                                        <div style="cursor:auto;color:#737F8D;font-family:Whitney, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif;font-size:16px;line-height:24px;text-align:center;">
-                                                                            <p><img src='https://res-console.cloudinary.com/dnkdk0ddu/media_explorer_thumbnails/125554ccc56e8b3965b04ed77c72b29d/detailed' alt="Party Wumpus" title="None" width="300" style="height: auto;"></p>
-                                                                            <div style="text-align:start;">
-                                                                                <h2 style="font-family: Whitney, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif;font-weight: 500;font-size: 20px;color: #4F545C;letter-spacing: 0.27px;">
-                                                                                    Hi ${req.body.userName}
-                                                                                </h2>
-                                                                                <p>Welcome to Skin Elegance! We're thrilled to have you join our community of skin care enthusiasts. To start exploring the best in skin care products, please verify your email address by clicking the link below.</p>
-                                                                            </div>
-                                                                        </div>
-                                                                    </td>
-                                                                </tr>
-                                                                <tr>
-                                                                    <td style="word-break:break-word;font-size:0px;padding:10px 25px;" align="center">
-                                                                        <table role="presentation" cellpadding="0" cellspacing="0" style="border-collapse:separate;" align="center" border="0">
-                                                                            <tbody>
-                                                                                <tr>
-                                                                                    <td style="border:none;border-radius:3px;color:white;cursor:auto;padding:15px 19px;" align="center" valign="middle" bgcolor="#FFC587">
-                                                                                        <a href="${req.protocol}://${req.headers.host}/auth/confirmEmail/${token}" style="text-decoration:none;line-height:100%;background:#FFC587;color:white;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:15px;font-weight:normal;text-transform:none;margin:0px;" target="_blank">
-                                                                                            Verify Email
-                                                                                        </a>
-                                                                                    </td>
-                                                                                </tr>
-                                                                            </tbody>
-                                                                        </table>
-                                                                    </td>
-                                                                </tr>
-                                                            </tbody>
-                                                        </table>
-                                                    </div>
-                                                    <!--[if mso | IE]>
-                                                    </td></tr></table>
-                                                    <![endif]-->
+    
+    <body style="background-color: #fafafa;">
+        <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%" align="center">
+            <tr>
+                <td>
+                    <div style="margin:0px auto;max-width:640px;background:#fafafa;">
+                        <table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:#fafafa;" align="center" border="0">
+                            <tbody>
+                                <tr>
+                                    <td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:40px 0px;">
+                                        <div style="margin:0px auto;max-width:640px;background:#fafafa;">
+                                            <table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:#fafafa;" align="center" border="0">
+                                                <tbody>
+                                                    <tr>
+                                                        <td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:40px 70px;">
+                                                            <div aria-labelledby="mj-column-per-100" class="mj-column-per-100 outlook-group-fix" style="vertical-align:top;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%;">
+                                                                <table role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                                                                    <tbody>
+                                                                        <tr>
+                                                                            <td style="word-break:break-word;font-size:0px;padding:0px 0px 20px;" align="left">
+                                                                                <div style="cursor:auto; font-family:Whitney, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif;font-size:16px;line-height:24px;text-align:center;">
+                                                                                    <p><img src="https://res-console.cloudinary.com/dnkdk0ddu/media_explorer_thumbnails/125554ccc56e8b3965b04ed77c72b29d/detailed" alt="Party Wumpus" title="None" width="300" style="height: auto;"></p>
+                                                                                    <div style="text-align:start;">
+                                                                                        <h2 style="font-family: Whitney, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif;font-weight: 500;font-size: 20px;color: #4F545C;letter-spacing: 0.27px;">Hi ${user.userName}</h2>
+                                                                                        <p>Welcome to Skin Elegance! We're thrilled to have you join our community of skin care enthusiasts. To start exploring the best in skin care products, please verify your email address by clicking the link below.</p>
+                                                                                    </div>
+                                                                                </div>
+                                                                            </td>
+                                                                        </tr>
+                                                                        <tr>
+                                                                            <td style="word-break:break-word;font-size:0px;padding:10px 25px;" align="center">
+                                                                                <table role="presentation" cellpadding="0" cellspacing="0" style="border-collapse:separate;" align="center" border="0">
+                                                                                    <tbody>
+                                                                                        <tr>
+                                                                                            <td style="border:none;border-radius:3px;color:white;cursor:auto;padding:15px 19px;" align="center" valign="middle" bgcolor="#46D7D4">
+                                                                                                <a href="${req.protocol}://${req.headers.host}/auth/confirmEmail/${token}" style="text-decoration:none;line-height:100%;background:#46D7D4;color:white;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:15px;font-weight:normal;text-transform:none;margin:0px;" target="_blank">
+                                                                                                    Verify Email
+                                                                                                </a>
+                                                                                            </td>
+                                                                                        </tr>
+                                                                                    </tbody>
+                                                                                </table>
+                                                                            </td>
+                                                                        </tr>
+                                                                    </tbody>
+                                                                </table>
+                                                            </div><!--[if mso | IE]>
+                                                            </td>
+                                                        </tr>
+                                                    </table>
+                                                <![endif]-->
                                                 </td>
                                             </tr>
                                         </tbody>
                                     </table>
                                 </div>
-                                <!--[if mso | IE]>
-                                </td></tr></table>
-                                <![endif]-->
                             </td>
                         </tr>
                     </tbody>
                 </table>
             </div>
-            <!--[if mso | IE]>
-            </td></tr></table>
-            <![endif]-->
-        </div>
+        </td>
+    </tr>
+    </table>
     </body>
-    `);
+    
+    </html>`
+    );
     
     const createUser = await UserModel.create({ userName, email, password: hashPassword, image: { secure_url, public_id },slug,phoneNumber,address });
     if (!createUser) {
