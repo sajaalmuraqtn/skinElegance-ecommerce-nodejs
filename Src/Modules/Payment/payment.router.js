8b3686d3e76be53b0686a9630f03c91659233a5e
sajaalmuraqtn <sajaalmuraqtn@gmail.com>
2024-05-31 21:17:33 +0300

    some edit

--- Src/Modules/Payment/payment.router.js
+++ Src/Modules/Payment/payment.router.js
@@ -7,7 +7,7 @@ import { asyncHandler } from "../../Services/errorHandling.js";
 const router=Router();
 
 router.post('/AddPayment' ,auth(roles.User),validation(validators.addPayment) ,asyncHandler(PaymentMethodController.createPaymentMethod));
-router.get('/:paymentMethodId' ,auth(Object.values(roles)),validation(validators.getSpecificPaymentMethod) ,asyncHandler(PaymentMethodController.getSpecificPaymentMethod));
+router.get('/getSpecificPaymentMethod/:paymentMethodId' ,auth(Object.values(roles)),validation(validators.getSpecificPaymentMethod) ,asyncHandler(PaymentMethodController.getSpecificPaymentMethod));
 router.get('/getPaymentMethods' ,auth(roles.User) ,asyncHandler(PaymentMethodController.getPaymentMethod));
 
 export default router;
